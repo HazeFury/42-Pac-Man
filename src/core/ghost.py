54d37edd641992cc2227a18928a38353fc73867d
gchmilew <g.chmile@gmail.com>
2026-09-23 18:12:22 +0200

    flee AI and flee asset

--- src/core/ghost.py
+++ src/core/ghost.py
@@ -1,5 +1,6 @@
-from core.maze import Maze
+import random
 from collections import deque
+from core.maze import Maze
 
 
 class Ghost:
@@ -95,47 +96,63 @@ class Ghost:
         maze = map
         target_x, target_y = x, y
         start = (self.x, self.y)
-
-        if self.ghost_type == "PINKY":
-            target_x, target_y = x, y
-            p_dir = direction
-            if p_dir == "UP":
-                target_y = max(0, target_y - 2)
-            elif p_dir == "DOWN":
-                target_y = min(maze.h - 1, target_y + 2)
-            elif p_dir == "LEFT":
-                target_x = max(0, target_x - 2)
-            elif p_dir == "RIGHT":
-                target_x = min(maze.w - 1, target_x + 2)
-
-        if self.ghost_type == "INKY":
-            target_x, target_y = x, y
-            p_dir = direction
-            g_x = blinky.x
-            g_y = blinky.y
-            pivot_x = target_x
-            pivot_y = target_y
-            if p_dir == "UP":
-                pivot_y = target_y - 2
-            elif p_dir == "DOWN":
-                pivot_y = target_y + 2
-            elif p_dir == "LEFT":
-                pivot_x = target_x - 2
-            elif p_dir == "RIGHT":
-                pivot_x = target_x + 2
-            raw_target_x = 2 * pivot_x - g_x
-            raw_target_y = 2 * pivot_y - g_y
-            target_x = max(0, min(maze.w - 1, raw_target_x))
-            target_y = max(0, min(maze.h - 1, raw_target_y))
-
-        if self.ghost_type == "CLYDE":
-            target_x, target_y = x, y
-            distance = (self.x - target_x) ** 2 + (
-                self.y - target_y
-            ) ** 2
-            if distance < 32:
-                target_x = 0
-                target_y = maze.h - 1
+        if self.state == "CHASE":
+            if self.ghost_type == "PINKY":
+                target_x, target_y = x, y
+                p_dir = direction
+                if p_dir == "UP":
+                    target_y = max(0, target_y - 2)
+                elif p_dir == "DOWN":
+                    target_y = min(maze.h - 1, target_y + 2)
+                elif p_dir == "LEFT":
+                    target_x = max(0, target_x - 2)
+                elif p_dir == "RIGHT":
+                    target_x = min(maze.w - 1, target_x + 2)
+            if self.ghost_type == "INKY":
+                target_x, target_y = x, y
+                p_dir = direction
+                g_x = blinky.x
+                g_y = blinky.y
+                pivot_x = target_x
+                pivot_y = target_y
+                if p_dir == "UP":
+                    pivot_y = target_y - 2
+                elif p_dir == "DOWN":
+                    pivot_y = target_y + 2
+                elif p_dir == "LEFT":
+                    pivot_x = target_x - 2
+                elif p_dir == "RIGHT":
+                    pivot_x = target_x + 2
+                raw_target_x = 2 * pivot_x - g_x
+                raw_target_y = 2 * pivot_y - g_y
+                target_x = max(0, min(maze.w - 1, raw_target_x))
+                target_y = max(0, min(maze.h - 1, raw_target_y))
+            if self.ghost_type == "CLYDE":
+                target_x, target_y = x, y
+                distance = (self.x - target_x) ** 2 + (
+                    self.y - target_y
+                ) ** 2
+                if distance < 32:
+                    target_x = 0
+                    target_y = maze.h - 1
+        if self.state == "FRIGHTENED":
+            possible_moves = []
+            current_dist = (self.x - x) ** 2 + (self.y - y) ** 2
+            flee_moves = []
+            for dx, dy, direction in moves:
+                nx = self.x + dx
+                ny = self.y + dy
+                if 0 <= nx < maze.w and 0 <= ny < maze.h:
+                    if not maze.grid[self.y][self.x].wall[direction]:
+                        possible_moves.append((nx, ny))
+                        new_dist = (nx - x) ** 2 + (ny - y) ** 2
+                        if new_dist > current_dist:
+                            flee_moves.append((nx, ny))
+            if flee_moves:
+                self.x, self.y = random.choice(flee_moves)
+            elif possible_moves:
+                self.x, self.y = random.choice(possible_moves)
+            return
 
         end = (target_x, target_y)
         queue = deque([start])
